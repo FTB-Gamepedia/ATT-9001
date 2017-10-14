5c68b37fd5a265ecccac29d8f379114bdcbfffd6
Eric Schneider <xbony22@gmail.com>
2017-10-14 09:33:36 -0400

    Caps-friendly now (some lang files have different names now)

--- lib/att9001/att9001.rb
+++ lib/att9001/att9001.rb
@@ -22,9 +22,9 @@ params = {
 
 loop do
   params["continue"] = continue
-  
+
   res = CLIENT.post(params)
-  
+
   res["query"]["tiles"].each do |entry|
     tilesheet[entry["name"]] = entry["id"]
   end
@@ -42,36 +42,36 @@ File.open("resources/#{MOD}/en_US.lang", "r").each do |line|
   next if line.match(/(.+)=(.+)\n/).nil?
   prop = line.sub(/=(.+)\n/, "")
   local = line.sub(/(.+)=/, "").sub(/\r/, "").sub(/\n/, "")
-  
+
   base[prop] = local if !tilesheet[local].nil?
 end
 
 TOKEN = CLIENT.post({action: "query", meta: "tokens"})["query"]["tokens"]["csrftoken"] #TODO: maybe fix MW butt
 
 Dir.glob("resources/#{MOD}/*.lang").each do |file|
-  next if file == "resources/#{MOD}/en_US.lang"
-  LANGUAGES[file.sub(/resources\/#{MOD}\/([\w_]+)\.lang/, '\1')].each do |code|
+  next if file == "resources/#{MOD}/en_US.lang" || file == "resources/#{MOD}/en_us.lang"
+  LANGUAGES[file.sub(/resources\/#{MOD}\/([\w_]+)\.lang/, '\1').downcase].each do |code|
     lang_tilesheet = {} # XX name => id (based existing translated tilesheet)
     params = {
       action: "query",
       list: "tiletranslations",
       tslang: code
     }
-    
+
     tilesheet.each do |en_entry, id|
       params["tsid"] = id
-      
+
       res = CLIENT.post(params)
       res["query"]["tiles"].each do |entry|
         lang_tilesheet[entry["display_name"]] = entry["entry_id"]
       end
     end
-    
+
     File.open(file, "r").each do |line|
       next if line.match(/(.+)=(.+)\n/).nil?
       prop = line.sub(/=(.+)\n/, "")
       local = line.sub(/(.+)=/, "").sub(/\r/, "").sub(/\n/, "")
-      
+
       if base[prop] != local && lang_tilesheet[local].nil? && !tilesheet[base[prop]].nil?
         params = {
           action: "translatetile",
@@ -80,7 +80,7 @@ Dir.glob("resources/#{MOD}/*.lang").each do |file|
           tslang: code,
           tsname: local
         }
-        
+
         puts "#{tilesheet[base[prop]]}; #{base[prop]} (#{prop}) => #{local} (#{code})"
         CLIENT.post(params)
       end
